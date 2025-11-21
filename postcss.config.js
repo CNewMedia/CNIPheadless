b7e8e30354486e647e6bd456deea857be13fa46a
CNewMedia <38378545+CNewMedia@users.noreply.github.com>
2025-11-21 21:40:17 +0100

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,9 @@
-module.exports = { plugins: { tailwindcss: {}, autoprefixer: {} } };
+import tailwindcss from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
+
+/** @type {import('postcss-load-config').Config} */
+const config = {
+  plugins: [tailwindcss, autoprefixer],
+};
+
+export default config;
